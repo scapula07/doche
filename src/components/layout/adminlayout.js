243233526c2199051caaede4d1231bab5ad74345
scapula07 <bartholomewonogwu@yahoo.com>
2022-05-29 20:34:59 +0100

    first commit

---
+++ src/components/layout/adminlayout.js
@@ -0,0 +1,31 @@
+import SideBar  from "../sidebar"
+import Header from "../header"
+
+const AdminLayout =({children})=>{
+
+  
+return(
+   <div className="m-auto ">
+      <div className="grid grid-cols-4">
+          <div>
+              <SideBar cname="text-center">
+                  <span className="block text-lg shadow-lg">Admin Panel</span>
+                  <ol>
+                    <li>
+
+                  </ol>
+              </SideBar>
+          </div>
+          <div className="col-span-3">
+
+          </div>
+      </div>
+      {children}
+   </div>
+
+)
+
+
+}
+
+export default AdminLayout
